aa8de2dc40f6b197a7245508b6c0d8fc4c2649c6
Luong Vo <luong@nimblehq.co>
2021-09-05 17:33:58 +0700

    [#5] Set blur background for login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_survey/pages/widgets/blur_background.dart';
+import 'package:flutter_survey/pages/widgets/dimmed_background.dart';
 import 'package:flutter_survey/resouces/assets.dart';
 
 class LoginPage extends StatelessWidget {
@@ -16,47 +18,54 @@ class LoginPage extends StatelessWidget {
               ),
             ),
           ),
+          BlurBackground(),
+          DimmedBackground(
+            colors: [
+              Colors.black.withOpacity(0.2),
+              Colors.black.withOpacity(0.8),
+              Colors.black,
+            ],
+            stops: [0.0, 0.6, 1.0],
+          ),
           Padding(
-            padding: EdgeInsets.all(32.0),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CupertinoTextField(
-                    autofocus: true,
-                    keyboardType: TextInputType.emailAddress,
-                    maxLength: 100,
-                    // TODO localization https://github.com/luongvo/flutter-survey/issues/25
-                    placeholder: "Email",
-                    textInputAction: TextInputAction.next,
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  CupertinoTextField(
-                    maxLength: 100,
-                    obscureText: true,
-                    placeholder: "Password",
-                    textInputAction: TextInputAction.done,
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  Flexible(
-                    child: FractionallySizedBox(
-                      widthFactor: 1.0,
-                      child: CupertinoButton(
-                        color: Colors.blue,
-                        padding: EdgeInsets.all(0),
-                        onPressed: () {
-                          // TODO login https://github.com/luongvo/flutter-survey/issues/7
-                        },
-                        child: Text('Log in'),
-                      ),
+            padding: EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CupertinoTextField(
+                  autofocus: true,
+                  keyboardType: TextInputType.emailAddress,
+                  maxLength: 100,
+                  // TODO localization https://github.com/luongvo/flutter-survey/issues/25
+                  placeholder: "Email",
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                CupertinoTextField(
+                  maxLength: 100,
+                  obscureText: true,
+                  placeholder: "Password",
+                  textInputAction: TextInputAction.done,
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Flexible(
+                  child: FractionallySizedBox(
+                    widthFactor: 1.0,
+                    child: CupertinoButton(
+                      color: Colors.blue,
+                      padding: EdgeInsets.all(0),
+                      onPressed: () {
+                        // TODO login https://github.com/luongvo/flutter-survey/issues/7
+                      },
+                      child: Text('Log in'),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
