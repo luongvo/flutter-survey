59560c8c483051d1f92e637c57332d42306bd5e4
Luong Vo <luong@nimblehq.co>
2021-09-05 17:11:26 +0700

    [#5] Set background for login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,52 +1,65 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_survey/resouces/assets.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Padding(
-        padding: EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CupertinoTextField(
-                autofocus: true,
-                keyboardType: TextInputType.emailAddress,
-                maxLength: 100,
-                // TODO localization https://github.com/luongvo/flutter-survey/issues/25
-                placeholder: "Email",
-                textInputAction: TextInputAction.next,
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(Assets.bgLogin),
+                fit: BoxFit.cover,
               ),
-              SizedBox(
-                height: 16.0,
-              ),
-              CupertinoTextField(
-                maxLength: 100,
-                obscureText: true,
-                placeholder: "Password",
-                textInputAction: TextInputAction.done,
-              ),
-              SizedBox(
-                height: 16.0,
-              ),
-              Flexible(
-                child: FractionallySizedBox(
-                  widthFactor: 1.0,
-                  child: CupertinoButton(
-                    color: Colors.blue,
-                    padding: EdgeInsets.all(0),
-                    onPressed: () {
-                      // TODO login https://github.com/luongvo/flutter-survey/issues/7
-                    },
-                    child: Text('Log in'),
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.all(32.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CupertinoTextField(
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    maxLength: 100,
+                    // TODO localization https://github.com/luongvo/flutter-survey/issues/25
+                    placeholder: "Email",
+                    textInputAction: TextInputAction.next,
+                  ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  CupertinoTextField(
+                    maxLength: 100,
+                    obscureText: true,
+                    placeholder: "Password",
+                    textInputAction: TextInputAction.done,
+                  ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  Flexible(
+                    child: FractionallySizedBox(
+                      widthFactor: 1.0,
+                      child: CupertinoButton(
+                        color: Colors.blue,
+                        padding: EdgeInsets.all(0),
+                        onPressed: () {
+                          // TODO login https://github.com/luongvo/flutter-survey/issues/7
+                        },
+                        child: Text('Log in'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/resouces/assets.dart
@@ -0,0 +1,5 @@
+class Assets {
+  const Assets._();
+
+  static const String bgLogin = "assets/images/bg_login.jpg";
+}
