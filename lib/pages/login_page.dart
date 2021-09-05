e0823be40aea3c1a6b8b6e9a3cbb46b19bbd4c6e
Luong Vo <luong@nimblehq.co>
2021-09-05 22:54:34 +0700

    [#5] Add Nimble logo on login screen

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_survey/pages/widgets/blur_background.dart';
 import 'package:flutter_survey/pages/widgets/dimmed_background.dart';
 import 'package:flutter_survey/resouces/assets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      resizeToAvoidBottomInset: false,
       child: Stack(
         children: [
           Container(
@@ -30,46 +32,58 @@ class LoginPage extends StatelessWidget {
           Padding(
             padding: EdgeInsets.all(24.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                CupertinoTextField(
-                  autofocus: true,
-                  keyboardType: TextInputType.emailAddress,
-                  maxLength: 100,
-                  // TODO localization https://github.com/luongvo/flutter-survey/issues/25
-                  placeholder: "Email",
-                  textInputAction: TextInputAction.next,
+                Expanded(
+                  child: SvgPicture.asset(Assets.icNimbleLogo),
                 ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                CupertinoTextField(
-                  maxLength: 100,
-                  obscureText: true,
-                  placeholder: "Password",
-                  textInputAction: TextInputAction.done,
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Flexible(
-                  child: FractionallySizedBox(
-                    widthFactor: 1.0,
-                    child: CupertinoButton(
-                      color: Colors.blue,
-                      padding: EdgeInsets.all(0),
-                      onPressed: () {
-                        // TODO login https://github.com/luongvo/flutter-survey/issues/7
-                      },
-                      child: Text('Log in'),
-                    ),
-                  ),
+                _buildLoginForm(),
+                Expanded(
+                  child: SizedBox.shrink(),
                 ),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildLoginForm() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CupertinoTextField(
+          autofocus: true,
+          keyboardType: TextInputType.emailAddress,
+          maxLength: 100,
+          // TODO localization https://github.com/luongvo/flutter-survey/issues/25
+          placeholder: "Email",
+          textInputAction: TextInputAction.next,
+        ),
+        SizedBox(
+          height: 16.0,
+        ),
+        CupertinoTextField(
+          maxLength: 100,
+          obscureText: true,
+          placeholder: "Password",
+          textInputAction: TextInputAction.done,
+        ),
+        SizedBox(
+          height: 16.0,
+        ),
+        Container(
+          width: double.infinity,
+          child: CupertinoButton(
+            color: Colors.blue,
+            padding: EdgeInsets.all(0),
+            onPressed: () {
+              // TODO login https://github.com/luongvo/flutter-survey/issues/7
+            },
+            child: Text('Log in'),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/resouces/assets.dart
+++ lib/resouces/assets.dart
@@ -2,4 +2,5 @@ class Assets {
   const Assets._();
 
   static const String bgLogin = "assets/images/bg_login.jpg";
+  static const String icNimbleLogo = "assets/icons/ic_nimble_logo.svg";
 }
