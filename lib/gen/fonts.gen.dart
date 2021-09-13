58018129936e580bc4a0066a89ac539d3721702c
Luong Vo <luong@nimblehq.co>
2021-09-14 02:35:36 +0700

    [#30] Add app assets into git for better build speed

---
+++ lib/gen/fonts.gen.dart
@@ -0,0 +1,11 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+class FontFamily {
+  FontFamily._();
+
+  static const String neuzeit = 'Neuzeit';
+  static const String neuzeitHeavy = 'Neuzeit-Heavy';
+}
