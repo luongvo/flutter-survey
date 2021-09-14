495800c9c26d7740c7e5c260ab68561a28744025
Luong Vo <luong@nimblehq.co>
2021-09-15 01:20:22 +0700

    [#6] Persist token result into SharedPreferences

--- lib/di/di.config.dart
+++ lib/di/di.config.dart
@@ -6,28 +6,38 @@
 
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
+import 'package:shared_preferences/shared_preferences.dart' as _i8;
 
-import '../api/oauth_service.dart' as _i6;
+import '../api/oauth_service.dart' as _i7;
 import '../api/repository/oauth_repository.dart' as _i4;
+import '../local/shared_preference_helper.dart' as _i5;
 import '../usecase/login_use_case.dart' as _i3;
-import 'module/network_module.dart' as _i7;
+import 'module/local_module.dart' as _i10;
+import 'module/network_module.dart' as _i9;
 import 'provider/dio_provider.dart'
-    as _i5; // ignore_for_file: unnecessary_lambdas
+    as _i6; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
-_i1.GetIt $initGetIt(_i1.GetIt get,
-    {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
+Future<_i1.GetIt> $initGetIt(_i1.GetIt get,
+    {String? environment, _i2.EnvironmentFilter? environmentFilter}) async {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
   final networkModule = _$NetworkModule();
-  gh.factory<_i3.LoginUseCase>(
-      () => _i3.LoginUseCase(get<_i4.OauthRepository>()));
-  gh.singleton<_i5.DioProvider>(_i5.DioProvider());
-  gh.singleton<_i6.OauthService>(
-      networkModule.provideOauthService(get<_i5.DioProvider>()));
+  final localModule = _$LocalModule();
+  gh.factory<_i3.LoginUseCase>(() => _i3.LoginUseCase(
+      get<_i4.OauthRepository>(), get<_i5.SharedPreferencesHelper>()));
+  gh.singleton<_i6.DioProvider>(_i6.DioProvider());
+  gh.singleton<_i7.OauthService>(
+      networkModule.provideOauthService(get<_i6.DioProvider>()));
+  await gh.singletonAsync<_i8.SharedPreferences>(() => localModule.sharedPref,
+      preResolve: true);
+  gh.singleton<_i5.SharedPreferencesHelper>(
+      _i5.SharedPreferencesHelperImpl(get<_i8.SharedPreferences>()));
   gh.singleton<_i4.OauthRepository>(
-      _i4.OauthRepositoryImpl(get<_i6.OauthService>()));
+      _i4.OauthRepositoryImpl(get<_i7.OauthService>()));
   return get;
 }
 
-class _$NetworkModule extends _i7.NetworkModule {}
+class _$NetworkModule extends _i9.NetworkModule {}
+
+class _$LocalModule extends _i10.LocalModule {}
--- lib/usecase/login_use_case.dart
+++ lib/usecase/login_use_case.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:flutter_survey/api/exception/network_exceptions.dart';
 import 'package:flutter_survey/api/repository/oauth_repository.dart';
+import 'package:flutter_survey/local/shared_preference_helper.dart';
+import 'package:flutter_survey/models/oauth_token.dart';
 import 'package:flutter_survey/usecase/base/base_use_case.dart';
 import 'package:injectable/injectable.dart';
 
@@ -15,17 +17,27 @@ class LoginInput {
 @Injectable()
 class LoginUseCase extends UseCase<void, LoginInput> {
   final OauthRepository _repository;
+  final SharedPreferencesHelper _sharedPreferencesHelper;
 
-  const LoginUseCase(this._repository);
+  const LoginUseCase(this._repository, this._sharedPreferencesHelper);
 
   @override
   Future<Result<void>> call(LoginInput input) {
-    // TODO persist token result
     return _repository
         .login(email: input.email, password: input.password)
-        .then((value) =>
-            Success(null) as Result<void>) // ignore: unnecessary_cast
+        .then((value) => _persistTokenData(value))
         .onError<NetworkExceptions>(
             (err, stackTrace) => Failed(UseCaseException(err, null)));
+  }
+
+  Result<dynamic> _persistTokenData(OAuthToken data) {
+    _sharedPreferencesHelper.saveTokenType(data.tokenType);
+    _sharedPreferencesHelper.saveAccessToken(data.accessToken);
+    _sharedPreferencesHelper.saveRefreshToken(data.refreshToken);
+
+    _sharedPreferencesHelper.saveTokenExpiration(
+        data.expiresIn * 1000 + DateTime.now().millisecondsSinceEpoch);
+
+    return Success(null);
   }
 }
