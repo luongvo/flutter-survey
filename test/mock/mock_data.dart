6c6bc4ccc16adecbaee1adac6d0bdeb73d922ec0
Luong Vo <luong@nimblehq.co>
2021-10-13 01:25:38 +0700

    [#53] Setup unit test with mockito + add login_usecase_test

---
+++ test/mock/mock_data.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_survey/api/repository/oauth_repository.dart';
+import 'package:flutter_survey/usecase/base/base_use_case.dart';
+import 'package:flutter_survey/usecase/login_use_case.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([OAuthRepository, LoginUseCase, UseCaseException])
+void main() {}
---
+++ test/usecase/login_use_case_test.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_survey/api/exception/network_exceptions.dart';
+import 'package:flutter_survey/models/oauth_token.dart';
+import 'package:flutter_survey/usecase/base/base_use_case.dart';
+import 'package:flutter_survey/usecase/login_use_case.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../test/mock/mock_data.mocks.dart';
+import '../fake/fake_shared_preference_helper.dart';
+
+void main() {
+  group('Login use case', () {
+    late MockOAuthRepository mockRepository;
+    late FakeSharedPreferencesHelper fakeSharePref;
+    late LoginUseCase loginUseCase;
+
+    setUp(() {
+      mockRepository = MockOAuthRepository();
+      fakeSharePref = FakeSharedPreferencesHelper();
+      loginUseCase = LoginUseCase(mockRepository, fakeSharePref);
+    });
+
+    test(
+        'When logging in with valid email and password, it returns Success result',
+        () async {
+      when(mockRepository.login(
+              email: anyNamed('email'), password: anyNamed('password')))
+          .thenAnswer((_) async => OAuthToken(
+                accessToken: "accessToken",
+                tokenType: "tokenType",
+                expiresIn: 111,
+                refreshToken: "refreshToken",
+              ));
+      final result = await loginUseCase
+          .call(LoginInput(email: 'email', password: 'password'));
+
+      expect(result, isA<Success>());
+    });
+
+    test(
+        'When logging in with incorrect email or password, it returns Failed result',
+        () async {
+      when(mockRepository.login(
+              email: anyNamed('email'), password: anyNamed('password')))
+          .thenAnswer((_) => Future.error(
+                NetworkExceptions.unauthorisedRequest(),
+              ));
+      final result = await loginUseCase
+          .call(LoginInput(email: 'email', password: 'password'));
+
+      expect(result, isA<Failed>());
+      expect((result as Failed).exception.networkExceptions,
+          NetworkExceptions.unauthorisedRequest());
+    });
+  });
+}
