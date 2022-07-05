e7d537fe66ad56b2db5e358507fcabd82696db31
Luong Vo <vodailuong@gmail.com>
2022-07-06 00:05:31 +0700

    [#16] Fetch latest survey detail

--- lib/pages/surveydetail/survey_detail_view_model.dart
+++ lib/pages/surveydetail/survey_detail_view_model.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_survey/models/survey_detail.dart';
 import 'package:flutter_survey/pages/surveydetail/survey_detail_state.dart';
 import 'package:flutter_survey/pages/uimodel/survey_ui_model.dart';
+import 'package:flutter_survey/usecase/base/base_use_case.dart';
 import 'package:flutter_survey/usecase/get_survey_detail_use_case.dart';
 import 'package:rxdart/subjects.dart';
 
@@ -19,6 +21,23 @@ class SurveyDetailViewModel extends StateNotifier<SurveyDetailState> {
     state = const SurveyDetailState.loading();
     _surveySubject.add(survey);
     state = const SurveyDetailState.success();
+
+    // Fetch latest survey detail
+    state = const SurveyDetailState.loading();
+    final result = await _getSurveyDetailUseCase.call(GetSurveyDetailInput(
+      surveyId: survey.id,
+    ));
+    if (result is Success<SurveyDetail>) {
+      final surveyDetail = result.value;
+      _surveySubject.add(SurveyUiModel.fromSurveyDetail(survey, surveyDetail));
+      state = const SurveyDetailState.success();
+    } else {
+      _handleError(result as Failed);
+    }
+  }
+
+  _handleError(Failed result) {
+    state = SurveyDetailState.error(result.getErrorMessage());
   }
 
   @override
