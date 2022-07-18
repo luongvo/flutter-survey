4fb9f2162710c5f51812bd3570d36d443a2eb4b3
Luong Vo <vodailuong@gmail.com>
2022-07-18 13:30:20 +0700

    [#22] Auto navigate to Home screen after 3 seconds, no animation implementation yet

--- lib/pages/completion/completion_page.dart
+++ lib/pages/completion/completion_page.dart
@@ -3,9 +3,15 @@ import 'package:flutter_survey/extensions/build_context_ext.dart';
 import 'package:flutter_survey/gen/assets.gen.dart';
 import 'package:flutter_survey/gen/colors.gen.dart';
 
+const _DELAY_TIME = 3;
+
 class CompletionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: _DELAY_TIME), () async {
+      Navigator.of(context).popUntil((route) => route.isFirst);
+    });
+
     return Container(
       color: ColorName.blackRussian,
       child: Column(
