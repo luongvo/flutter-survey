80bcddeca2cb014f42303fccc6bb9c9b66d2be18
Luong Vo <vodailuong@gmail.com>
2022-07-18 13:06:00 +0700

    [#22] Implement Survey Submit Completion ui

---
+++ lib/pages/completion/completion_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_survey/extensions/build_context_ext.dart';
+import 'package:flutter_survey/gen/assets.gen.dart';
+import 'package:flutter_survey/gen/colors.gen.dart';
+
+class CompletionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: ColorName.blackRussian,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(child: SizedBox.shrink()),
+          Assets.images.bgCompletion.image(),
+          Text(
+            context.localization.completionThanks,
+            style:
+                Theme.of(context).textTheme.headline5!.copyWith(fontSize: 28.0),
+            textAlign: TextAlign.center,
+          ),
+          Expanded(child: SizedBox.shrink()),
+        ],
+      ),
+    );
+  }
+}
