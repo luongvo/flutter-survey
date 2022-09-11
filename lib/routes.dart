b92fd286461f7cbe22915c0657961a9f25d5e412
Luong Vo <vodailuong@gmail.com>
2022-09-11 17:14:54 +0700

    [#8] Implement Reset Password screen ui

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_survey/pages/completion/completion_page.dart';
 import 'package:flutter_survey/pages/home/home_page.dart';
+import 'package:flutter_survey/pages/resetpassword/reset_password_page.dart';
 import 'package:flutter_survey/pages/startup_page.dart';
 import 'package:flutter_survey/pages/surveydetail/survey_detail_page.dart';
 
@@ -8,12 +9,14 @@ class Routes {
   Routes._();
 
   static const String startup = "/";
+  static const String resetPassword = "/reset-password";
   static const String home = "/home";
   static const String survey = "/survey";
   static const String completion = "/completion";
 
   static final routes = <String, WidgetBuilder>{
     startup: (BuildContext context) => StartupPage(),
+    resetPassword: (BuildContext context) => ResetPasswordPage(),
     home: (BuildContext context) => HomePage(),
     survey: (BuildContext context) => SurveyDetailPage(),
     completion: (BuildContext context) => CompletionPage(),
