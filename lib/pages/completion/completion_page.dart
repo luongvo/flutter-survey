2b93e850951ab34c0a0c649e9d6672423af9f33a
Luong Vo <vodailuong@gmail.com>
2022-09-11 10:57:45 +0700

    [#23] Show Lottie completion animation and navigate back to Home after finished

--- lib/pages/completion/completion_page.dart
+++ lib/pages/completion/completion_page.dart
@@ -1,24 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_survey/extensions/build_context_ext.dart';
-import 'package:flutter_survey/gen/assets.gen.dart';
 import 'package:flutter_survey/gen/colors.gen.dart';
+import 'package:lottie/lottie.dart';
 
-const _DELAY_TIME = 3;
+class CompletionPage extends StatefulWidget {
+  const CompletionPage({Key? key}) : super(key: key);
 
-class CompletionPage extends StatelessWidget {
+  @override
+  State<CompletionPage> createState() => _CompletionPageState();
+}
+
+class _CompletionPageState extends State<CompletionPage>
+    with TickerProviderStateMixin {
+  late final AnimationController _animationController =
+      AnimationController(vsync: this)
+        ..addStatusListener((status) {
+          if (status == AnimationStatus.completed) {
+            Navigator.of(context).popUntil((route) => route.isFirst);
+          }
+        });
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: _DELAY_TIME), () async {
-      Navigator.of(context).popUntil((route) => route.isFirst);
-    });
-
     return Container(
       color: ColorName.blackRussian,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Expanded(child: SizedBox.shrink()),
-          Assets.images.bgCompletion.image(),
+          Lottie.asset(
+            'assets/lotties/completion_success.json',
+            width: 200,
+            height: 200,
+            fit: BoxFit.fill,
+            controller: _animationController,
+            onLoaded: (composition) {
+              _animationController
+                ..duration = composition.duration
+                ..forward();
+            },
+          ),
           Text(
             context.localization.completionThanks,
             style:
