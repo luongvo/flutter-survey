e60506d37a99ee35cb3bffdb0b6d79370b01516b
Luong Vo <vodailuong@gmail.com>
2022-10-03 02:27:53 +0700

    [#72] Prepare env for the integration test such as getIt injection, Hive initialization, etc

--- integration_test/login_test.dart
+++ integration_test/login_test.dart
@@ -1,4 +1,7 @@
+import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_survey/di/di.dart';
+import 'package:flutter_survey/local/database/hive.dart';
 import 'package:flutter_survey/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -11,6 +14,16 @@ void main() {
       child: SurveyApp(),
     );
   }
+
+  setUpAll(() async {
+    FlutterConfig.loadValueForTesting({
+      'REST_API_ENDPOINT': 'REST_API_ENDPOINT',
+      'BASIC_AUTH_CLIENT_ID': 'CLIENT_ID',
+      'BASIC_AUTH_CLIENT_SECRET': 'CLIENT_SECRET',
+    });
+    await initHive();
+    await configureInjection();
+  });
 
   group('Login Test', () {
     final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
